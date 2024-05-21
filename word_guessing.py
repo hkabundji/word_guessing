4cd6414f12585d9d1d37f1a5cd9ed7f8871903a5
hkabundji <127061009+hkabundji@users.noreply.github.com>
2024-05-21 12:11:29 -0400

    Add files via upload

--- word_guessing.py
+++ word_guessing.py
@@ -4,11 +4,16 @@ import random
 secret_word = "iphone" 
 
 tries = 0
-tries_limit = 3
+word_guess = 0
+limit = 3
+letter_guess_limit = 3
+word_guess_limit = 3
+letter_guess = []
+
 print("Welcome to my word guessing game!\n")
 
 letter_guess = input("Guess a letter:\n")
-if tries < tries_limit:
+if tries < letter_guess_limit and word_guess < word_guess_limit:
     letter_guess = input("Guess a letter:\n")
     print(f"You guessed the letter '{letter_guess}'")
 
@@ -18,3 +23,11 @@ if letter_guess != secret_word:
 else: 
     ("correct")
 
+if word_guess >= word_guess_limit:
+    print("yikes you ran out of guesses")
+
+if letter_guess >= letter_guess_limit:
+    print("oops no more guesses. Better luck next time!")
+
+if letter_guess == secret_word:
+    print("you did it yay!")  
